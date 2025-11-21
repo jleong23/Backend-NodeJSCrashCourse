ee12387280ae53fcadd024d43df55e3890e4235a
Jason Leong <jleongdh@gmail.com>
2025-11-21 10:32:08 +1030

    Completed User model

---
+++ src/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose"; // Schema means structure
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowerCase: true,
+      trim: true, // trimming white spaces in string ( matilda mong: will become matildamong)
+      minLength: 1,
+      maxLength: 30,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+      maxLength: 50,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowerCase: true,
+      trim: true, // trimming white spaces in string ( matilda mong: will become matildamong)
+      minLength: 1,
+      maxLength: 30,
+    },
+  },
+  { timestamps: true }
+);
+export const User = mongoose.model("User", userSchema); // very important syntax
